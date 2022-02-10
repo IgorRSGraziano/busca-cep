163bd5c0a790ded6e0c4817ce43354d63daf7051
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-02-10 15:59:17 -0300

    refact: placed async and await do in zipCodeValidation function

--- script.js
+++ script.js
@@ -3,27 +3,29 @@ const btn = document.querySelector('#search')
 
 btn.addEventListener('click', zipCodeValidation)
 
-
-function zipCodeValidation() {
+async function zipCodeValidation(event) {
     event.preventDefault()
 
     const zipCodeSearch = document.querySelector('#cep').value
-    const zipCodeAPI = fetch(`https://viacep.com.br/ws/${zipCodeSearch}/json/`)
     const resultArea = document.querySelectorAll('.result span')
 
-    zipCodeAPI.then(r => r.json()).then(resultJSON => {
+
+    try {
+        const zipCodeAPI = await fetch(`https://viacep.com.br/ws/${zipCodeSearch}/json/`)
+
+        const zipCodeJSON = await zipCodeAPI.json()
         resultArea.forEach(el => {
             const spanLocate = el.classList.value
-            if (resultJSON[spanLocate]) {
-                el.innerText = resultJSON[spanLocate]
+            if (zipCodeJSON[spanLocate]) {
+                el.innerText = zipCodeJSON[spanLocate]
             }
         })
-    }).catch(() => {
+    } catch {
         resultArea.forEach(el => {
             el.innerText = ''
         })
         const zipCodeArea = document.querySelector('.cep')
         zipCodeArea.innerText = 'Não localizado'
-    })
+    }
 }
 
