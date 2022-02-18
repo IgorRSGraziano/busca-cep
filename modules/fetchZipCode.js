51a3ab94ce9db647648bff0c8a211a4100e9b5bf
IgorRSGraziano <igorrsgraziano@gmail.com>
2022-02-18 12:22:43 -0300

    chore: add module JS structure

---
+++ modules/fetchZipCode.js
@@ -0,0 +1,31 @@
+export default function fetchZipCode() {
+    const btn = document.querySelector('#search')
+
+    btn.addEventListener('click', zipCodeValidation)
+
+    async function zipCodeValidation(event) {
+        event.preventDefault()
+
+        const zipCodeSearch = document.querySelector('#cep').value
+        const resultArea = document.querySelectorAll('.result span')
+
+
+        try {
+            const zipCodeAPI = await fetch(`https://viacep.com.br/ws/${zipCodeSearch}/json/`)
+
+            const zipCodeJSON = await zipCodeAPI.json()
+            resultArea.forEach(el => {
+                const spanLocate = el.classList.value
+                if (zipCodeJSON[spanLocate]) {
+                    el.innerText = zipCodeJSON[spanLocate]
+                }
+            })
+        } catch {
+            resultArea.forEach(el => {
+                el.innerText = ''
+            })
+            const zipCodeArea = document.querySelector('.cep')
+            zipCodeArea.innerText = 'Não localizado'
+        }
+    }
+}
---
+++ modules/script.js
@@ -0,0 +1,3 @@
+import fetchZipCode from "./fetchZipCode.js";
+
+fetchZipCode()
